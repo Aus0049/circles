7ab5ecb7040a880640a706e7f2a89f2bbc7ec228
赵彬宇 <zhaobinyu@qijiayoudao.com>
2018-04-19 17:47:18 +0800

    client header组件重构

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -10,61 +10,21 @@ import './header.scss';
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 
-// const Item = Popover.Item;
-
 class Header extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             isPopoverShow: false
         };
-        // this.handleVisibleChange = this.handleVisibleChange.bind(this);
-        // this.handleSelect = this.handleSelect.bind(this);
-    }
-    getOverLayDOM () {
-        // const {username, phone} = this.props.site;
-        const overlayDOM = [];
-
-        // if(username && phone){
-        //     overlayDOM.push(
-        //         <Item key="signOut" value="signOut" icon={<Icon type="sign-out"/>} >注销</Item>,
-        //         <Item key="add" value="add" icon={<Icon type="plus"/>} >发朋友圈</Item>
-        //     );
-        // } else {
-        //     overlayDOM.push(
-        //         <Item key="signIn" value="signIn" icon={<Icon type="sign-in"/>} >登录</Item>,
-        //         <Item key="signUp" value="signUp" icon={<Icon type="user-plus"/>} >注册</Item>
-        //     );
-        // }
-        // overlayDOM.push(
-        //     <Item key="signOut" value="signOut" icon={<Icon type="sign-out"/>} >注销</Item>,
-        //     <Item key="add" value="add" icon={<Icon type="plus"/>} >发朋友圈</Item>
-        // );
-
-        return overlayDOM;
     }
     render () {
-        const {isPopoverShow} = this.state;
-        const overlayDOM = this.getOverLayDOM();
-
         return (
             <div className="header-container">
-                <div className="header-left">
-                    <Icon type="bars" />
-                </div>
-                <div className="header-title">朋友圈</div>
-                <div className="header-right">
-                    {/*<Popover*/}
-                        {/*mask*/}
-                        {/*visible={isPopoverShow}*/}
-                        {/*overlay={overlayDOM}*/}
-                        {/*// onVisibleChange={this.handleVisibleChange}*/}
-                        {/*// onSelect={this.handleSelect}*/}
-                    {/*>*/}
-                        {/*<div>*/}
-                            {/*<Icon type="ellipsis-h" />*/}
-                        {/*</div>*/}
-                    {/*</Popover>*/}
+                <div className="header-box">
+                    <div className="logo">CIRCLES</div>
+                    <ul className="link-list">
+                        <li className="item"><Icon type="home"/>首页</li>
+                    </ul>
                 </div>
             </div>
         );
