99259f80a8a2e57524abd5a6615ba5836c88cb90
赵彬宇 <zhaobinyu@qijiayoudao.com>
2018-04-22 17:10:51 +0800

    client 首页panel样式

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -7,7 +7,8 @@ export default class Home extends Component {
     render() {
         return (
             <div className="home-container">
-                <div className="user-container">
+
+                <div className="user-container panel">
                     <img src="" alt="" className="user-head"/>
                     <div className="user-info">
                         <p className="info username">zby</p>
@@ -15,6 +16,32 @@ export default class Home extends Component {
                         <p className="info birthday">1993-12-08</p>
                         <p className="info birthday">北京</p>
                     </div>
+                </div>
+
+                <div className="feed-container panel">
+                    <ul className="feed-list">
+                        <li className="article-item">
+                            <div className="author-box">
+                                <img src="" alt="" className="author-head"/>
+                                <p className="author-name">aby</p>
+                                <p className="author-bio">的撒的撒</p>
+                            </div>
+                            <div className="content">
+                                sadsadsadsadsada撒
+                            </div>
+                            <ul className="article-options">
+                                <li className="option">
+                                    评论
+                                </li>
+                                <li className="option">
+                                    点赞
+                                </li>
+                                <li className="option">
+                                    转发
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
             </div>
         )
