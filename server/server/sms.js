095eb2dce69b5c908241f957f589f6a015238a3f
赵彬宇 <zhaobinyu@qijiayoudao.com>
2018-05-17 12:18:27 +0800

    server 验证码发送成功

--- server/server/sms.js
+++ server/server/sms.js
@@ -2,9 +2,9 @@
  * Created by Aus on 2018/5/4.
  */
 import smsClient, {companyName, signUpTemplateCode} from '../common/ali_sms';
-import loggerConfig, {smsServerLogger} from '../common/logger';
+import {smsServerLogger} from '../common/logger';
 import Captcha from '../proxy/captcha';
-import {createJSONResponseData} from '../common/tool';
+import tool from '../common/tool';
 // 只向外暴露流程 内部方法不暴露
 
 // 短信验证码类别：
@@ -20,40 +20,39 @@ function create4BitsCaptcha () {
 
 // 注册获取验证码
 async function getSignUpCaptcha (ctx) {
-    console.log('ok');
-    // // 1.创建简单验证码
-    // const captchaCode = create4BitsCaptcha();
-    //
-    // // 2. 获取手机号
-    // const {mobile} = req.body;
-    //
-    // // 3. 结果入库
-    // const captcha = await Captcha.save(captchaCode, mobile);
-    //
-    // // 4. 交给验证码服务发送验证码
-    // smsClient.sendSMS({
-    //     PhoneNumbers: mobile,
-    //     SignName: companyName,
-    //     TemplateCode: signUpTemplateCode,
-    //     TemplateParam: `{"code":"${captchaCode}"}`
-    // })
-    //     .then((smsRes)=>{
-    //         // 验证码发送成功
-    //         smsServerLogger.info(`${mobile}验证码发送成功！id: ${captcha.captcha_id}, code: ${captchaCode}`);
-    //         // 验证码id和验证码返回前端
-    //         res.send(createJSONResponseData(true, '验证码发送成功！', {
-    //             captchaId: captcha.captcha_id,
-    //             captchaCode: captcha.captcha_code,
-    //         }));
-    //     })
-    //     .catch((smsRes)=>{
-    //         // 验证码发送失败
-    //         smsServerLogger.error(`${mobile}验证码发送失败！`);
-    //         smsServerLogger.error(`smsRes: ${smsRes}`);
-    //         res.send(createJSONResponseData(false, '验证码发送失败！'));
-    //     });
+    // 1.创建简单验证码
+    const captchaCode = create4BitsCaptcha();
+
+    // 2. 获取手机号
+    const {mobile} = ctx.request.body;
+
+    // 3. 结果入库
+    const captcha = await Captcha.save(captchaCode, mobile);
+
+    // 4. 交给验证码服务发送验证码
+    await smsClient.sendSMS({
+        PhoneNumbers: mobile,
+        SignName: companyName,
+        TemplateCode: signUpTemplateCode,
+        TemplateParam: `{"code":"${captchaCode}"}`
+    })
+        .then((smsRes)=>{
+            // 验证码发送成功
+            smsServerLogger.info(`${mobile}验证码发送成功！id: ${captcha.captcha_id}, code: ${captchaCode}`);
+            // 验证码id和验证码返回前端
+            ctx.body = tool.createJSONResponseData(true, '验证码发送成功！', {
+                captchaId: captcha.captcha_id,
+                captchaCode: captcha.captcha_code,
+            });
+        })
+        .catch((smsRes)=>{
+            // 验证码发送失败
+            smsServerLogger.error(`${mobile}验证码发送失败！`);
+            smsServerLogger.error(`smsRes: ${smsRes}`);
+            ctx.body = tool.createJSONResponseData(false, '验证码发送失败！');
+        });
 }
 
 module.exports = {
     getSignUpCaptcha
-}
+};
