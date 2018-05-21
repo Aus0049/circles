d3e8f0ccf5cfc6a7e791aae10e525a0fa12a14f2
赵彬宇 <zhaobinyu@qijiayoudao.com>
2018-05-21 19:49:57 +0800

    server 使用类的方法修改入口文件

--- server/app.js
+++ server/app.js
@@ -2,35 +2,73 @@
  * Created by Aus on 2018/3/9.
  */
 import koa from 'koa';
+import mongoose from 'mongoose';
 import config from './config/';
 import routers from './route/';
 import logger from './common/logger';
 import applyMiddleware from './middleware';
 
-//创建koa服务器应用
-const app = new koa();
+class Application {
+    constructor(){
+        //创建koa服务器应用
+        const app = new koa();
 
-app.keys = ['circles_aus'];
+        // 挂载数据
+        this.app = app;
+        this.config = config;
 
-// 1. 中间件配置
-applyMiddleware(app);
-
-
-
-
-
-// 2. 配置数据库
-require('./config/db_config');
-
-// 3. 配置路由
-app.use(routers);
-
-// 4. 监听端口
-app.listen(config.serverPort, function(err) {
-    if (err) {
-        logger.error('server start err: ' + err);
+        console.log(this);
+        // 执行
+        this.create();
     }
+    create () {
+        // 1. 中间件配置
+        this.middlewareConfig();
 
-    logger.info('server running on port: ' + config.serverPort);
-});
+        // 2. 配置数据库
+        this.dbConfig();
 
+        // 3. 配置路由
+        this.routerConfig();
+
+        // 4. 监听
+        this.listen();
+    }
+    middlewareConfig () {
+        const {app} = this;
+
+        app.keys = ['circles_aus'];
+
+        applyMiddleware(app);
+
+    }
+    dbConfig () {
+        const {config} = this;
+
+        mongoose.connect(config.db, {poolSize: 20})
+            .then(()=>{
+                logger.info('数据库连接成功！🍺');
+            })
+            .catch((err)=>{
+                logger.error('数据库连接失败！😢');
+                logger.error(err);
+            });
+    }
+    routerConfig(){
+        // 3. 配置路由
+        this.app.use(routers);
+    }
+    listen (){
+        const {app, config} = this;
+        // 4. 监听端口
+        app.listen(config.serverPort, function(err) {
+            if (err) {
+                logger.error('server start err: ' + err);
+            }
+
+            logger.info('server running on port: ' + config.serverPort);
+        });
+    }
+}
+
+export default new Application();
