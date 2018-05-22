72af4bb2d87193194e80ff1fc7c3417658b7ea6e
赵彬宇 <zhaobinyu@qijiayoudao.com>
2018-05-22 20:58:21 +0800

    server 思考了很多 有很多想法 重头再来搭建一下

--- server/app.js
+++ server/app.js
@@ -1,74 +1,99 @@
 /**
  * Created by Aus on 2018/3/9.
  */
-import koa from 'koa';
-import mongoose from 'mongoose';
-import config from './config/';
-import routers from './route/';
-import logger from './common/logger';
-import applyMiddleware from './middleware';
+// import koa from 'koa';
+// import mongoose from 'mongoose';
+//
+// import config from './config/';
+// // import routers from './route/';
+// import Controller, {instance} from './controller/';
+//
+// import logger from './common/logger';
+// import applyMiddleware from './middleware';
 
-class Application {
-    constructor(){
-        //创建koa服务器应用
-        const app = new koa();
-
-        // 挂载数据
-        this.app = app;
-        this.config = config;
-
-        console.log(this);
-        // 执行
-        this.create();
-    }
-    create () {
-        // 1. 中间件配置
-        this.middlewareConfig();
-
-        // 2. 配置数据库
-        this.dbConfig();
-
-        // 3. 配置路由
-        this.routerConfig();
-
-        // 4. 监听
-        this.listen();
-    }
-    middlewareConfig () {
-        const {app} = this;
-
-        app.keys = ['circles_aus'];
-
-        applyMiddleware(app);
-
-    }
-    dbConfig () {
-        const {config} = this;
-
-        mongoose.connect(config.db, {poolSize: 20})
-            .then(()=>{
-                logger.info('数据库连接成功！🍺');
-            })
-            .catch((err)=>{
-                logger.error('数据库连接失败！😢');
-                logger.error(err);
-            });
-    }
-    routerConfig(){
-        // 3. 配置路由
-        this.app.use(routers);
-    }
-    listen (){
-        const {app, config} = this;
-        // 4. 监听端口
-        app.listen(config.serverPort, function(err) {
-            if (err) {
-                logger.error('server start err: ' + err);
-            }
-
-            logger.info('server running on port: ' + config.serverPort);
-        });
-    }
-}
+// class Application {
+//     constructor(){
+//         //创建koa服务器应用
+//         const app = new koa();
+//
+//         // 挂载数据
+//         this.app = app;
+//         this.config = config;
+//         this.logger = logger;
+//         // 执行
+//         this.create();
+//     }
+//     create () {
+//         // 1. 中间件配置
+//         this.middlewareConfig();
+//     }
+//     middlewareConfig () {
+//         // 配置中间件
+//         this.app.keys = ['circles_aus'];
+//
+//         applyMiddleware.call(this);
+//     }
+// }
+// class Application {
+//     constructor(){
+//         //创建koa服务器应用
+//         const app = new koa();
+//
+//         // 挂载数据
+//         this.app = app;
+//         this.config = config;
+//         // 执行
+//         this.create();
+//     }
+//     create () {
+//         // 1. 中间件配置
+//         this.middlewareConfig();
+//
+//         // 2. 配置数据库
+//         this.dbConfig();
+//
+//         // 3. 配置路由
+//         this.routerConfig();
+//
+//         // 4. 监听
+//         this.listen();
+//     }
+//     middlewareConfig () {
+//         const {app} = this;
+//
+//         app.keys = ['circles_aus'];
+//
+//         applyMiddleware(app);
+//     }
+//     dbConfig () {
+//         const {config} = this;
+//
+//         mongoose.connect(config.db, {poolSize: 20})
+//             .then(()=>{
+//                 logger.info('数据库连接成功！🍺');
+//             })
+//             .catch((err)=>{
+//                 logger.error('数据库连接失败！😢');
+//                 logger.error(err);
+//             });
+//     }
+//     routerConfig(){
+//         // 将Controller类实例化，在于route匹配
+//
+//
+//         // this.app.use(routers);
+//     }
+//     listen (){
+//         const {app, config} = this;
+//         // 4. 监听端口
+//         app.listen(config.serverPort, function(err) {
+//             if (err) {
+//                 logger.error('server start err: ' + err);
+//             }
+//
+//             logger.info('server running on port: ' + config.serverPort);
+//         });
+//     }
+// }
 
 export default new Application();
--- server/controller/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Aus on 2018/5/18.
- */
-import support from './support';
-
-export default {
-    support
-}
